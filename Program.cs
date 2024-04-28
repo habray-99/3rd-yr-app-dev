005e98dd9aa5f101deec06532356a1e334d0dd6e
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-28 19:00:10 +0545

    Update authentication configuration and dependencies

--- Program.cs
+++ Program.cs
@@ -1,137 +1,134 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApplication6.Areas.Identity.Data;
-using WebApplication6.Controllers;
 
-namespace WebApplication6
+namespace WebApplication6;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var connectionString = builder.Configuration.GetConnectionString("WebApplication6ContextConnection") ?? throw new InvalidOperationException("Connection string 'WebApplication6ContextConnection' not found.");
+        var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("WebApplication6ContextConnection") ??
+                               throw new InvalidOperationException(
+                                   "Connection string 'WebApplication6ContextConnection' not found.");
 
-            builder.Services.AddDbContext<WebApplication6Context>(options => options.UseSqlServer(connectionString));
+        builder.Services.AddDbContext<WebApplication6Context>(options => options.UseSqlServer(connectionString));
 
-            builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<WebApplication6Context>();
+        builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<WebApplication6Context>();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
-            builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen();
 
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
 
-            builder.Services.AddAuthentication()
-                .AddGoogle(googleOptions =>
-                {
-                    googleOptions.ClientId = config["GoogleKeys:ClientId"] ?? throw new InvalidOperationException();
-                    googleOptions.ClientSecret = config["GoogleKeys:ClientSecret"] ?? throw new InvalidOperationException();
-                });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+        builder.Services.AddAuthentication()
+            .AddGoogle(googleOptions =>
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                googleOptions.ClientId = config["GoogleKeys:ClientId"] ?? throw new InvalidOperationException();
+                googleOptions.ClientSecret = config["GoogleKeys:ClientSecret"] ?? throw new InvalidOperationException();
+            })
+            .AddFacebook(opt =>
+            {
+                opt.ClientId = config["FacebookKeys:ClientId"] ?? throw new InvalidOperationException();
+                opt.ClientSecret = config["FacebookKeys:ClientSecret"] ?? throw new InvalidOperationException();
+            });
 
-                        if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-};
+        var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-            //app.Run();
+        if (app.Environment.IsDevelopment())
+        {
             app.UseSwagger();
             app.UseSwaggerUI();
+        }
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        ;
 
-                var roles = new[] { "Admin", "Blogger", "Surfer" };
-                foreach (var role in roles)
-                {
-                    if (!await roleManager.RoleExistsAsync(role))
-                    {
-                        try
-                        {
-                            await roleManager.CreateAsync(new IdentityRole(role));
-                        }
-                        catch (Exception ex)
-                        {
-                            // Log or handle the exception
-                            Console.WriteLine($"Failed to create role {role}: {ex.Message}");
-                        }
-                    }
-                }
-            }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<CustomUser>>();
+        app.UseRouting();
 
-                const string email = "iamadmin@admin.com";
-                const string password = "Test1234";
-                if (await userManager.FindByEmailAsync(email) == null)
-                {
-                    var user = new CustomUser()
-                    {
-                        UserName = email,
-                        Email = email,
-                        EmailConfirmed = true
-                    };
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            "default",
+            "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        //app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "Blogger", "Surfer" };
+            foreach (var role in roles)
+                if (!await roleManager.RoleExistsAsync(role))
                     try
                     {
-                        var result = await userManager.CreateAsync(user, password);
-                        if (result.Succeeded)
-                        {
-                            await userManager.AddToRoleAsync(user, "Admin");
-                        }
-                        else
-                        {
-                            foreach (var error in result.Errors)
-                            {
-                                // Log or handle each error
-                                Console.WriteLine(error.Description);
-                            }
-                        }
+                        await roleManager.CreateAsync(new IdentityRole(role));
                     }
                     catch (Exception ex)
                     {
                         // Log or handle the exception
-                        Console.WriteLine($"Failed to create user {email}: {ex.Message}");
+                        Console.WriteLine($"Failed to create role {role}: {ex.Message}");
                     }
+        }
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<CustomUser>>();
+
+            const string email = "iamadmin@admin.com";
+            const string password = "Test1234";
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var user = new CustomUser
+                {
+                    UserName = email,
+                    Email = email,
+                    EmailConfirmed = true
+                };
+                try
+                {
+                    var result = await userManager.CreateAsync(user, password);
+                    if (result.Succeeded)
+                        await userManager.AddToRoleAsync(user, "Admin");
+                    else
+                        foreach (var error in result.Errors)
+                            // Log or handle each error
+                            Console.WriteLine(error.Description);
+                }
+                catch (Exception ex)
+                {
+                    // Log or handle the exception
+                    Console.WriteLine($"Failed to create user {email}: {ex.Message}");
                 }
             }
-
-            // Start the application
-            await app.RunAsync();
         }
+
+        // Start the application
+        await app.RunAsync();
     }
 }

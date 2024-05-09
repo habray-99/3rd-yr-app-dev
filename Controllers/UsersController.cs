810ff07b72d6e23e625963d6cbe9d6b516507e06
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-05-09 08:37:52 +0545

    feat: Add new folder for user pages

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication6.Areas.Identity.Data;
 using WebApplication6.Models;
@@ -15,10 +21,29 @@ namespace WebApplication6.Controllers
         }
 
         // GET: Users
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    var users = await _context.Users.ToListAsync();
+        //    return View(users);
+        //}
+        public async Task<IActionResult> Index(string searchQuery)
         {
-            return View(await _context.Users.ToListAsync());
+            var users = from u in _context.Users
+                select u;
+
+            if (!String.IsNullOrEmpty(searchQuery))
+            {
+                users = users.Where(u => u.UserName.Contains(searchQuery) ||
+                                         u.Email.Contains(searchQuery) ||
+                                         u.PhoneNumber.Contains(searchQuery) ||
+                                         u.Address.Contains(searchQuery));
+            }
+
+            var results = await users.ToListAsync();
+
+            return View(results);
         }
+
 
         // GET: Users/Details/5
         public async Task<IActionResult> Details(string id)
@@ -28,14 +53,14 @@ namespace WebApplication6.Controllers
                 return NotFound();
             }
 
-            var customUser = await _context.Users
-               .FirstOrDefaultAsync(m => m.Id == id);
-            if (customUser == null)
+            var user = await _context.Users
+               .FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
             {
                 return NotFound();
             }
 
-            return View(customUser);
+            return View(user);
         }
 
         // GET: Users/Create
@@ -45,17 +70,21 @@ namespace WebApplication6.Controllers
         }
 
         // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount,Role,Address,ProfilePicture,Blogs,Comments,Reactions,Notifications,CommentReactions,BlogMetrics,UserMetrics")] CustomUser customUser)
+        public async Task<IActionResult> Create([Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount,Role,Address,ProfilePicture,Blogs,Comments,Reactions,Notifications,CommentReactions,BlogMetrics,UserMetrics")] CustomUser user)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(customUser);
+                var passwordHasher = new PasswordHasher<CustomUser>();
+                user.PasswordHash = passwordHasher.HashPassword(user, user.PasswordHash);
+                _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(customUser);
+            return View(user);
         }
 
         // GET: Users/Edit/5
@@ -66,20 +95,22 @@ namespace WebApplication6.Controllers
                 return NotFound();
             }
 
-            var customUser = await _context.Users.FindAsync(id);
-            if (customUser == null)
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
             {
                 return NotFound();
             }
-            return View(customUser);
+            return View(user);
         }
 
         // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount,Role,Address,ProfilePicture,Blogs,Comments,Reactions,Notifications,CommentReactions,BlogMetrics,UserMetrics")] CustomUser customUser)
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount,Role,Address,ProfilePicture,Blogs,Comments,Reactions,Notifications,CommentReactions,BlogMetrics,UserMetrics")] CustomUser user)
         {
-            if (id != customUser.Id)
+            if (id != user.Id)
             {
                 return NotFound();
             }
@@ -88,12 +119,12 @@ namespace WebApplication6.Controllers
             {
                 try
                 {
-                    _context.Update(customUser);
+                    _context.Update(user);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CustomUserExists(customUser.Id))
+                    if (!UserExists(user.Id))
                     {
                         return NotFound();
                     }
@@ -104,7 +135,7 @@ namespace WebApplication6.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(customUser);
+            return View(user);
         }
 
         // GET: Users/Delete/5
@@ -115,14 +146,14 @@ namespace WebApplication6.Controllers
                 return NotFound();
             }
 
-            var customUser = await _context.Users
-               .FirstOrDefaultAsync(m => m.Id == id);
-            if (customUser == null)
+            var user = await _context.Users
+               .FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
             {
                 return NotFound();
             }
 
-            return View(customUser);
+            return View(user);
         }
 
         // POST: Users/Delete/5
@@ -130,17 +161,17 @@ namespace WebApplication6.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            var customUser = await _context.Users.FindAsync(id);
-            if (customUser != null)
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
             {
-                _context.Users.Remove(customUser);
+                _context.Users.Remove(user);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CustomUserExists(string id)
+        private bool UserExists(string id)
         {
             return _context.Users.Any(e => e.Id == id);
         }

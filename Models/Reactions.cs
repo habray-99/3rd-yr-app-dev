6158af6b28443ef26372d020f3e6ae3a532d2a66
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-05-09 14:13:03 +0545

    refactor: Add CreateReactionNotification method to Reactions.cs

--- Models/Reactions.cs
+++ Models/Reactions.cs
@@ -19,5 +19,19 @@ namespace WebApplication6.Models
         public virtual CustomUser? User { get; set; }
         //[ForeignKey("ReactionTypeID")]
         public virtual ReactionType? ReactionType { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Today;
+
+        public void CreateReactionNotification(IdentityDBContext context, string blogOwnerUserId)
+        {
+            var notification = new Notification
+            {
+                UserID = blogOwnerUserId,
+                NotificationType = "New reaction on your blog post",
+                EntityID = this.BlogID
+            };
+
+            context.Notifications.Add(notification);
+            context.SaveChanges();
+        }
     }
 }

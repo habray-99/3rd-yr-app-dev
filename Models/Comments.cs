8a7ea8d3f9a5e5cbade1120affa35ea88c8f9427
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-05-09 14:12:35 +0545

    refactor: Add CreateCommentNotification method to Comments.cs

--- Models/Comments.cs
+++ Models/Comments.cs
@@ -14,12 +14,24 @@ namespace WebApplication6.Models
         public string? UserID { get; set; }
         [Required]
         public string? CommentText { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Today;
         //[ForeignKey("BlogID")]
         public virtual Blog? Blog { get; set; }
         //[ForeignKey("UserID")]
         public virtual CustomUser? User { get; set; }
-
         public ICollection<CommentReaction>? CommentReactions { get; set; }
+        public void CreateCommentNotification(IdentityDBContext context, string blogOwnerUserId)
+        {
+            var notification = new Notification
+            {
+                UserID = blogOwnerUserId,
+                NotificationType = "New comment on your blog post",
+                EntityID = this.BlogID
+            };
+
+            context.Notifications.Add(notification);
+            context.SaveChanges();
+        }
     }
 }
+

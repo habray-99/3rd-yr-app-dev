a04feb2a6172aa2757e11607fa0deb8a29c45a22
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-05-09 14:12:44 +0545

    refactor: Add DashboardService.cs with DashboardViewModel

---
+++ Models/DashboardService.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication6.Areas.Identity.Data;
+using WebApplication6.Models;
+
+public class DashboardViewModel
+{
+    public int? BlogCount { get; set; }
+    public int? UpvoteCount {get;set;}
+    public int? DownvoteCount {get;set;}
+    public int? CommentCount {get;set;}
+    public List<Blog> TopBlogPosts {get;set;}
+    public List<CustomUser> TopBloggers {get;set;}
+    public int? SelectedMonth { get; set; }
+    public int? SelectedYear { get; set; }
+}
